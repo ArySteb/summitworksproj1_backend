de91d5d57e2c6b52afbef3e65a55f15702579464
Steven Santos <stevensan0606@gmail.com>
2021-07-28 09:43:38 -0400

    initial commit

---
+++ src/main/java/com/aryansteven/summitworksproj/SummitworksprojApplication.java
@@ -0,0 +1,13 @@
+package com.aryansteven.summitworksproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitworksprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummitworksprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aryansteven/summitworksproj/SummitworksprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryansteven.summitworksproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummitworksprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
